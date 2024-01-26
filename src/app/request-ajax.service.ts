92e168f35b11ab2c546cb665756963820bdac62c
raulgodii <raulgodii13@gmail.com>
2024-01-26 09:29:37 +0100

    spinner

--- src/app/request-ajax.service.ts
+++ src/app/request-ajax.service.ts
@@ -9,28 +9,36 @@ export class RequestAJAXService {
   trendingCoins: any[] = [];
   searchResponse: any[] = [];
   coinData: any = null;
+  spinner: boolean = false;
 
   constructor(private http: HttpClient) {
   }
 
   getTrending() {
+    this.spinner = true;
     this.http.get("https://api.coingecko.com/api/v3/search/trending").subscribe((coins: any) => {
       this.trendingCoins = coins.coins;
       console.log(this.trendingCoins);
+      this.spinner = false;
     });
   }
 
   searchCoin(searchInput: any) {
+    this.spinner = true;
     this.http.get("https://api.coingecko.com/api/v3/search?query=" + searchInput).subscribe((searchResponse: any) => {
       this.searchResponse = searchResponse.coins;
       console.log(searchResponse.coins);
+      this.spinner = false;
     });
+    
   }
 
   getDataCoin(idCoin: any) {
+    this.spinner = true;
     this.http.get("https://api.coingecko.com/api/v3/coins/" + idCoin).subscribe((response: any) => {
       this.coinData = response;
       console.log(response);
+      this.spinner = false;
     });
   }
 }
