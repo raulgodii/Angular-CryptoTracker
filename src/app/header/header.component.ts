4ecf2aec9419fa955ac0774a6f5ba6aed01451c9
raulgodii <raulgodii13@gmail.com>
2024-01-27 10:36:13 +0100

    googleProvider

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { RequestAJAXService } from '../request-ajax.service';
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 @Component({
   selector: 'app-header',
@@ -12,43 +11,11 @@ import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 })
 export class HeaderComponent {
 
-  auth = getAuth();
-  provider = new GoogleAuthProvider();
-
   constructor(public requestAjax: RequestAJAXService) {
 
   }
 
   obtenerDatos() {
     this.requestAjax.obtenerDatosFirestore();
-  }
-
-  logGoogle() {
-    signInWithPopup(this.auth, this.provider)
-      .then((result) => {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        const credential = GoogleAuthProvider.credentialFromResult(result);
-        if (credential) {
-          const token = credential.accessToken;
-          // The signed-in user info.
-          const user = result.user;
-          // IdP data available using getAdditionalUserInfo(result)
-          // ...
-  
-
-        } else {
-          // Manejar el caso en que 'credential' sea nulo
-          console.error("Error: credential is null");
-        }
-      }).catch((error) => {
-        // Handle Errors here.
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // The email of the user's account used.
-        const email = error.customData.email;
-        // The AuthCredential type that was used.
-        const credential = GoogleAuthProvider.credentialFromError(error);
-        // ...
-      });
   }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 @Component({
   selector: 'app-login',
@@ -8,5 +9,36 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  auth = getAuth();
+  googleProvider = new GoogleAuthProvider();
+  
+  signInGoogle() {
+    console.log("entra");
+    signInWithPopup(this.auth, this.googleProvider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        if (credential) {
+          const token = credential.accessToken;
+          // The signed-in user info.
+          const user = result.user;
+          // IdP data available using getAdditionalUserInfo(result)
+          // ...
 
+
+        } else {
+          // Manejar el caso en que 'credential' sea nulo
+          console.error("Error: credential is null");
+        }
+      }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  }
 }
