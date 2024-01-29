4d72896cef3f443a87e7adaec4f18e509e26bae0
raulgodii <raulgodii13@gmail.com>
2024-01-29 18:30:28 +0100

    querySnapshot

--- src/app/firestore-control.service.ts
+++ src/app/firestore-control.service.ts
@@ -20,7 +20,7 @@ export class FirestoreControlService {
   getPortfolio(uid: any) {
     const portfolioCollection = collection(this.firestore, "portfolio");
     const queryByUid = query(portfolioCollection, where('uid', '==', uid));
-    
+
     onSnapshot(queryByUid, (snapshot) => {
       this.portfolio = snapshot.docs.map(doc => {
         return {
@@ -32,22 +32,35 @@ export class FirestoreControlService {
     });
   }
 
-  followCrypto(rank: any, thumb:any, name: any, symbol: any, cid: any, uid: any) {
-    addDoc(collection(this.db, "portfolio"), {
-      rank: rank,
-      thumb: thumb,
-      name: name,
-      symbol: symbol,
-      cid: cid,
-      uid: uid
-    }).then((docRef) => {
-      console.log("Document written with ID: ", docRef.id);
+  followCrypto(rank: any, thumb: any, name: any, symbol: any, cid: any, uid: any) {
+    const cryptoCollectionRef = collection(this.db, 'portfolio');
+
+    // Realizar una consulta para verificar si ya existe un documento con el mismo 'cid'
+    const q = query(cryptoCollectionRef, where('cid', '==', cid));
+
+    getDocs(q).then((querySnapshot) => {
+      if (querySnapshot.size === 0) {
+        // No hay documentos con los mismos 'cid' y 'uid', por lo que se puede agregar el documento
+        addDoc(cryptoCollectionRef, {
+          rank: rank,
+          thumb: thumb,
+          name: name,
+          symbol: symbol,
+          cid: cid,
+          uid: uid
+        }).then((docRef) => {
+          console.log("Document written with ID: ", docRef.id);
+        });
+      } else {
+        // Ya existe un documento con los mismos 'cid' y 'uid'
+        console.log("Document already exists");
+      }
     });
   }
 
   unfollowCrypto(id: any) {
-    
-    deleteDoc(doc(this.db, "portfolio", id)).then(() =>{
+
+    deleteDoc(doc(this.db, "portfolio", id)).then(() => {
       console.log("ID: " + id)
     });
   }
