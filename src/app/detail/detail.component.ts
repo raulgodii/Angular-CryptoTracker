f96c76315325ff6ed52f9367f28e462dbfa5e95f
raulgodii <raulgodii13@gmail.com>
2024-01-29 17:14:33 +0100

    detail

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -6,6 +6,8 @@ import { ActivatedRoute } from '@angular/router';
 import { RequestAJAXService } from '../request-ajax.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { FirestoreControlService } from '../firestore-control.service';
+import { UserControlService } from '../user-control.service';
 
 @Component({
   selector: 'app-detail',
@@ -18,7 +20,7 @@ export class DetailComponent implements OnInit{
   @Input() id:any;
   @Output() closeDetailEvent = new EventEmitter<string>();
 
-  constructor(public RequestAJAX:RequestAJAXService, private scroll:ViewportScroller, private router:ActivatedRoute){
+  constructor(public RequestAJAX:RequestAJAXService, private scroll:ViewportScroller, private router:ActivatedRoute, public firestoreService:FirestoreControlService, public userService: UserControlService){
     this.router.params.subscribe(params => {
       const id = params['id'];
       // Puedes hacer algo con el ID aquí, como cargar los datos asociados al detalle.
@@ -38,5 +40,9 @@ export class DetailComponent implements OnInit{
 
   closeDetail(){
     this.closeDetailEvent.emit();
+  }
+
+  followCrypto(rank: any, thumb:any, name: any, symbol: any, id:any){
+    this.firestoreService.followCrypto(rank, thumb, name, symbol, id, this.userService.user.uid);
   }
 }
